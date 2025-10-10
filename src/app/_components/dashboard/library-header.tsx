ce402833f44259e90a3bc46154849fa4d8f284d5
ronitrajfr <ronitrajofficial7@gmail.com>
2025-10-10 22:17:01 +0530

    added uploadthing

--- src/app/_components/dashboard/library-header.tsx
+++ src/app/_components/dashboard/library-header.tsx
@@ -15,11 +15,11 @@ const LibraryHeader = ({
       </h1>
       <p className="text-lg text-neutral-500">{description}</p>
       <div className="mb-6 flex space-x-6">
-        <button className="flex items-center gap-2 rounded-xl bg-stone-300 px-4 py-2 font-semibold text-stone-500">
+        <button className="flex items-center gap-2 rounded-xl bg-stone-300 px-4 py-2 font-semibold text-stone-500 hover:cursor-pointer">
           <p>Add book</p>
           <PlusCircle size={15} />
         </button>
-        <button className="flex items-center gap-2 rounded-xl bg-stone-300 px-4 py-2 font-semibold text-stone-500">
+        <button className="flex items-center gap-2 rounded-xl bg-stone-300 px-4 py-2 font-semibold text-stone-500 hover:cursor-pointer">
           <p>Edit</p>
           <Pencil size={15} />
         </button>
